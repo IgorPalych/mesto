40e7acaddcd62808e5f6ac3b6a50d244585d1f71
Igor Palych <pigori@mail.ru>
2022-11-02 14:28:26 +0300

    feat: вывести имя и род занятий в popup

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,23 @@
-const popupElement = document.querySelector('.popup');
 
-const editButton = document.querySelector('.profile__button_action_edit');
-const closeButton = document.querySelector('.popup__close');
+
+const popupElement = document.querySelector('.popup'); // получить ссылку на попап
+const editButton = document.querySelector('.profile__button_action_edit'); // получить ссылку на кнопку "Редактировать профиль"
+const closeButton = document.querySelector('.popup__close'); // получить ссылку на кнопку "Закрыть попап"
+
+const inputUser = document.querySelector('.form__item_el_name') // получить ссылку на поле формы "Имя пользователя"
+const inputJob = document.querySelector('.form__item_el_job') // получить ссылку на поле формы "Род деятельности пользователя"
+
+const userName = document.querySelector('.profile__user').textContent; // получить текст имени пользователя
+const userJob = document.querySelector('.profile__job').textContent; //получить текст рода занятий пользователя
+
+
+console.log(userName);
+console.log(userJob);
 
 function openPopup() {
   popupElement.classList.add('popup_opened');
+  inputUser.setAttribute('placeholder', userName);
+  inputJob.setAttribute('placeholder', userJob);
 }
 
 function closePopup(event) {
