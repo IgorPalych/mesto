ee38d1c1048c2bf8fcb22be2cfa205e270f5beed
Igor Palych <pigori@mail.ru>
2022-12-01 16:57:31 +0300

    feat: ПР-6: Добавить массив настроек

--- scripts/validate.js
+++ scripts/validate.js
@@ -6,75 +6,82 @@ function hasInvalidInput(inputList) {
 }
 
 // Включить / отключить кнопку формы
-function toggleButtonState(inputList, buttonElement) {
+function toggleButtonState(inputList, buttonElement, settings) {
   // Если есть хотя бы один невалидный инпут
   if (hasInvalidInput(inputList)) {
     // сделай кнопку неактивной
     buttonElement.setAttribute('disabled', true);
-    buttonElement.classList.add('form__submit_disabled');
+    buttonElement.classList.add(settings.inactiveButtonClass);
   } else {
     // иначе сделай кнопку активной
     buttonElement.removeAttribute('disabled');
-    buttonElement.classList.remove('form__submit_disabled');
+    buttonElement.classList.remove(settings.inactiveButtonClass);
   }
 };
 
 
 // Показать ошибку заполнения поля
-function showInputError(formElement, inputElement, errorMessage) {
+function showInputError(formElement, inputElement, errorMessage, settings) {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add('form__input_type_error');
+  inputElement.classList.add(settings.inputErrorClass);
   errorElement.textContent = errorMessage;
-  errorElement.classList.add('form__error_visible');
+  errorElement.classList.add(settings.errorClass);
 
 }
 
 
 // Убрать ошибку заполнения поля
-function hideInputError(formElement, inputElement) {
+function hideInputError(formElement, inputElement, settings) {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove('form__input_type_error');
+  inputElement.classList.remove(settings.inputErrorClass);
   errorElement.textContent = '';
-  errorElement.classList.remove('form__error_visible');
+  errorElement.classList.remove(settings.errorClass);
 }
 
 
 // Проверить валидность заполнения поля
-function isValid(formElement, inputElement) {
+function isValid(formElement, inputElement, settings) {
   if (!inputElement.validity.valid) {
     // Если поле не проходит валидацию, покажем ошибку
-    showInputError(formElement, inputElement, inputElement.validationMessage);
+    showInputError(formElement, inputElement, inputElement.validationMessage, settings);
   } else {
     // Если проходит, скроем
-    hideInputError(formElement, inputElement);
+    hideInputError(formElement, inputElement, settings);
   }
 };
 
 
 // Установить обработчики проверки на поля формы
-function setEventListener(formElement) {
-  const inputList = Array.from(formElement.querySelectorAll('.form__input'));
-  const buttonElement = formElement.querySelector('.form__submit');
+function setEventListener(formElement, settings) {
+  const inputList = Array.from(formElement.querySelectorAll(settings.inputSelector));
+  const buttonElement = formElement.querySelector(settings.submitButtonSelector);
   inputList.forEach(inputElement => {
     inputElement.addEventListener('input', () => {
-      isValid(formElement, inputElement);
-      toggleButtonState(inputList, buttonElement);
+      isValid(formElement, inputElement, settings);
+      toggleButtonState(inputList, buttonElement, settings);
     });
   });
-  toggleButtonState(inputList, buttonElement);
+  toggleButtonState(inputList, buttonElement, settings);
 }
 
 
 // Запустить установку обработчиков всем формам
-function enableValidation() {
-  const formList = Array.from(document.querySelectorAll('.form'));
+function enableValidation(settings) {
+  const formList = Array.from(document.querySelectorAll(settings.formSelector));
   formList.forEach(formElement => {
-    setEventListener(formElement);
+    setEventListener(formElement, settings);
   });
 }
 
 
-enableValidation();
+enableValidation({
+  formSelector: '.form', // готово
+  inputSelector: '.form__input', // готово
+  submitButtonSelector: '.form__submit',  // готово
+  inactiveButtonClass: 'form__submit_disabled',
+  inputErrorClass: 'form__input_type_error', // готово
+  errorClass: 'form__error_visible' // готово
+});
 
 
 
